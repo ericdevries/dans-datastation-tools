6ce961acf2423ae44765a9fcbebea31630aad8be
Eric de Vries <hro.mail@gmail.com>
2023-04-05 17:18:32 +0200

    Refactor a bit

--- src/datastation/scripts/notifications_cleanup.py
+++ src/datastation/scripts/notifications_cleanup.py
@@ -8,27 +8,51 @@ from datetime import timedelta
 from datastation.config import init
 
 
-def cleanup_notifications(dvndb_conn, days_old):
-    notifications_older_than = timedelta(days=days_old)
+class DataverseDBConnection:
+    def __init__(self, host, dbname, user, password):
+        self.host = host
+        self.dbname = dbname
+        self.user = user
+        self.password = password
+
+    def __enter__(self):
+        self.connection = psycopg.connect(
+            "host={} dbname={} user={} password={}".format(
+                self.host, self.dbname, self.user, self.password
+            )
+        )
+        return self.connection
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.close()
+
+
+def cleanup_notifications(conn, days_old):
     logging.info("Cleaning up notifications older than %s days", days_old)
+
+    notifications_older_than = timedelta(days=days_old)
 
     delete_statement = """
         delete
         from usernotification
         where senddate < now() - %s
     """
-    
-    with dvndb_conn.cursor() as dvndb_cursor:
+
+    with conn.cursor() as cursor:
         try:
-            result = dvndb_cursor.execute(delete_statement, (notifications_older_than,))
-            amount_deleted = result.rowcount
-            dvndb_conn.commit()
+            delete_result = cursor.execute(delete_statement, (notifications_older_than,))
+            amount_deleted = delete_result.rowcount
+            conn.commit()
 
             logging.info("Deleted %s notifications", amount_deleted)
-            
+
         except psycopg.DatabaseError as error:
             logging.error(error)
-            sys.exit("FATAL ERROR: problem accessing database with {} ".format(delete_statement))
+            sys.exit(
+                "FATAL ERROR: problem accessing database with {} ".format(
+                    delete_statement
+                )
+            )
 
 
 def positive_int(value):
@@ -37,30 +61,28 @@ def positive_int(value):
         raise argparse.ArgumentTypeError("value must be a number greater than zero")
     return ivalue
 
+
 def main():
     config = init()
-    parser = argparse.ArgumentParser(description='Clean up old notifications')
-    parser.add_argument("--days-old", help="Minimum amount of days old", type=positive_int, required=True)
+    parser = argparse.ArgumentParser(description="Clean up old notifications")
+    parser.add_argument(
+        "--days-old",
+        help="Minimum amount of days old",
+        type=positive_int,
+        required=True,
+    )
     args = parser.parse_args()
 
-    dvndb_conn = None
+    dvndb_conn = DataverseDBConnection(
+        config["dataverse"]["db"]["host"],
+        config["dataverse"]["db"]["dbname"],
+        config["dataverse"]["db"]["user"],
+        config["dataverse"]["db"]["password"],
+    )
 
-    try:
-        dvndb_conn = connect_to_database(config['dataverse']['db']['host'], config['dataverse']['db']['dbname'],
-                                         config['dataverse']['db']['user'], config['dataverse']['db']['password'])
-        cleanup_notifications(dvndb_conn, args.days_old)
-    except psycopg.DatabaseError as error:
-        logging.error(error)
-        sys.exit("FATAL ERROR: No connection with database established: {}".format(error))
-    finally:
-        if dvndb_conn is not None:
-            dvndb_conn.close()
+    with dvndb_conn as conn:
+        cleanup_notifications(conn, args.days_old)
 
 
-def connect_to_database(host, dbname, user: str, password: str):
-    return psycopg.connect(
-        "host={} dbname={} user={} password={}".format(host, dbname, user, password))
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
